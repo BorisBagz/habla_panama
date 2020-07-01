2769b343062b52f9a4b6b663de7de4f017a89b38
borisbagz <root@kaliSS.local>
2020-07-01 18:20:45 -0400

    separating functions of printing features

--- habla-panama.py
+++ habla-panama.py
@@ -8,10 +8,8 @@ HABLA PANAMA
 by: boris a.
 """
 
-#sys.setdefaultencoding('utf-8')
-
 # GLOBAL VARIABLES
-json_words = 'words.json'     #json file containing the questions
+json_words = 'words_test.json'     #json file containing the questions
 
 #read the json file and load it to the variable DATA
 def read_json_file(json_file):
@@ -19,18 +17,35 @@ def read_json_file(json_file):
         data = json.load(questions_file)
         return data
 
+#print only the word
+def print_word(data):
+    word = data["word"]
+    print("{0}".format(word))
+
+#print the word and its type
+def print_word_type(data):
+    word = data["word"]
+    type = data["type"]
+    print("{0} [{1}] :".format(word, type))
+
+#print a single entry: word, type and definition
+def print_single_entry(data):
+    definition = []
+    print_word_type(data)
+    nb_def = len(data["definition"])
+    for j in range(nb_def):
+        def_tmp = data["definition"][str(j)]
+        print("{0}. {1}".format(j+1, def_tmp))
+
+#printing all the entries of the dictionary
+def print_all_entries(data):
+    for i in range(len(data)):
+        print_single_entry(data[i])
+        print("--------")
+
 def main():
     data = read_json_file(json_words)
-    for i in range(len(data)):
-        definition = []
-        word = data[i]["word"]
-        type = data[i]["type"]
-        print("{0} [{1}] :".format(word, type))
-        nb_def = len(data[i]["definition"])
-        for j in range(nb_def):
-            def_tmp = data[i]["definition"][str(j)]
-            print("{0}. {1}".format(j+1, def_tmp))
-        print("--------")
+    print_all_entries(data)
 
 if __name__ == '__main__':
     main()
