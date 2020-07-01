69cabcdcef5fcf052e9a66dcbd330238b055596c
borisbagz <root@kaliSS.local>
2020-07-01 18:31:55 -0400

    adding the add entries function beta

--- habla-panama.py
+++ habla-panama.py
@@ -9,13 +9,38 @@ by: boris a.
 """
 
 # GLOBAL VARIABLES
-json_words = 'words_test.json'     #json file containing the questions
+my_json_words = 'words_test.json'     #json file containing the questions
 
 #read the json file and load it to the variable DATA
 def read_json_file(json_file):
-    with open(json_file) as questions_file:
-        data = json.load(questions_file)
+    with open(json_file) as words_file:
+        data = json.load(words_file)
         return data
+
+# function to add to JSON
+def write_json(data, filename=my_json_words):
+    with open(filename,'w') as f:
+        json.dump(data, f, indent=4)
+
+#adding entries to the dictionary
+def adding_entries(words_file):
+    tmp_word = "simi"
+    tmp_type = "s"
+    tmp_definition = "simi's definition"
+    #adding process
+    with open(words_file) as json_file:
+        data = json.load(json_file)
+        # json object to be appended
+        new_entry = {   "word": tmp_word,
+                        "type": tmp_type,
+                        "definition":
+                        {
+                            "0": tmp_definition
+                        }
+                    }
+        # appending data to file
+        data.append(new_entry)
+    write_json(data)
 
 #print only the word
 def print_word(data):
@@ -44,7 +69,8 @@ def print_all_entries(data):
         print("--------")
 
 def main():
-    data = read_json_file(json_words)
+    adding_entries(my_json_words)
+    data = read_json_file(my_json_words)
     print_all_entries(data)
 
 if __name__ == '__main__':
